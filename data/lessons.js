e1fba1c747068878d18557e29f61d3faaaa26693
jishnu <jd5@hybrhind.com>
2025-12-30 21:52:05 +0530

    added videos url

--- data/lessons.js
+++ data/lessons.js
@@ -1,7 +1,7 @@
 export const lessons = [
-  { id: 1, title: "Introduction to Mentalism", duration: "6 min", streamId: "2d4859d7aeb17588b13865eebf8afdf7" },
-  { id: 2, title: "Psychology of Perception", duration: "8 min", streamId: "VIDEO_ID_2" },
-  { id: 3, title: "Psychological Forces", duration: "7 min", streamId: "VIDEO_ID_3" },
+  { id: 1, title: "Introduction ", duration: "2 min", streamId: "2d4859d7aeb17588b13865eebf8afdf7" },
+  { id: 2, title: "which hand coin effect", duration: "4 min", streamId: "edd663acf7cb671ce925e8e0cabf356c" },
+  { id: 3, title: "Six cards match", duration: "8 min", streamId: "7d2494ce9df1e389b40714927ba6a168" },
   { id: 4, title: "Mind Reading Basics", duration: "6 min", streamId: "VIDEO_ID_4" },
   { id: 5, title: "Cold Reading Fundamentals", duration: "7 min", streamId: "VIDEO_ID_5" },
   { id: 6, title: "Practical Mentalism Routine", duration: "6 min", streamId: "VIDEO_ID_6" }
