abcc8a1045ce9966c8e247f5886f052c72cad79f
jishnu <jd5@hybrhind.com>
2025-12-30 22:08:18 +0530

    cors fix according to frontend deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
     origin: [
       "http://localhost:8081",
       "http://localhost:5173",
-      "https://della-mind-academy-97.vercel.app",
+      "https://dellacourse.vercel.app/",
     ],
     credentials: true,
   })
