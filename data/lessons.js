a668f6c72a59cb3b506b8088d52d9ccdf6f5c094
jishnu <jd5@hybrhind.com>
2025-12-31 15:26:08 +0530

    Added full videos

--- data/lessons.js
+++ data/lessons.js
@@ -2,7 +2,7 @@ export const lessons = [
   { id: 1, title: "Introduction ", duration: "2 min", streamId: "2d4859d7aeb17588b13865eebf8afdf7" },
   { id: 2, title: "which hand coin effect", duration: "4 min", streamId: "edd663acf7cb671ce925e8e0cabf356c" },
   { id: 3, title: "Six cards match", duration: "8 min", streamId: "7d2494ce9df1e389b40714927ba6a168" },
-  { id: 4, title: "Mind Reading Basics", duration: "6 min", streamId: "VIDEO_ID_4" },
-  { id: 5, title: "Cold Reading Fundamentals", duration: "7 min", streamId: "VIDEO_ID_5" },
-  { id: 6, title: "Practical Mentalism Routine", duration: "6 min", streamId: "VIDEO_ID_6" }
+  { id: 4, title: "Center Tear", duration: "9 min", streamId: "efda2d762778d279d96cca2155737cea" },
+  { id: 5, title: "Finding Person", duration: "5 min", streamId: "3dcca989d1485fab9588be4e2067ab04" },
+  { id: 6, title: "Prediction", duration: "7 min", streamId: "f4ab00d0a885bcf5da96159970a62e9b" }
 ];
